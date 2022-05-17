2006cc2c2c34580c5c1264a4bac65a1447036d47
Joseph Eck <jlemxe@gmail.com>
2022-05-17 18:52:08 -0500

    test4

--- static/js/charts.js
+++ static/js/charts.js
@@ -55,7 +55,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("/static/data/samples.json").then((data) => {
+  d3.json("../data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
 
