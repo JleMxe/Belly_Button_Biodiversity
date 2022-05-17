0496b2c32536464fe27443be8b0542d1b6808f3e
Joseph Eck <jlemxe@gmail.com>
2022-05-17 18:13:52 -0500

    Mod 12 Challenge Deliverables

---
+++ static/js/plots.js
@@ -0,0 +1,46 @@
+//define the init function
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("/static/data/samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
+// Call the init function
+init();
+
+// Define the function that gets called when a new name is selected
+// this.value from index.htm = newSample
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+//  buildCharts(newSample);
+}
+
+//
+function buildMetadata(sample) {
+  d3.json("/static/data/samples.json").then((data) => {
+    var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
